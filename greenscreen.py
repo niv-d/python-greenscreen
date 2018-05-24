d79aec5222c509b4fc85027cade7a6e260ad822e
dmrat <dmratc@dmratcliffe.com>
2018-05-23 19:06:10 -0500

    GUI Running -not implemented

--- greenscreen.py
+++ greenscreen.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-
 #Webcam Number
 webcam = 0
 
---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from gsui import Ui_MainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__ (self, parent = None):
+        super (MainWindow, self).__init__ ()
+        self.ui = Ui_MainWindow ()
+        self.ui.setupUi (self)
+
+def main():
+    app = QtWidgets.QApplication (sys.argv)
+    m = MainWindow ()
+    m.show ()
+    sys.exit (app.exec_ () )
+
+
+if __name__ == '__main__':
+    main ()
